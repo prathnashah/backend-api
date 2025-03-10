3c65c63096184a6c2cef5337beb7d4173c145e4c
prathnashah <147828690+prathnashah@users.noreply.github.com>
2025-03-10 21:32:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def get_sensor_data():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
